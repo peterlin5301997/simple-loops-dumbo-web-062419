c9ae1a45464f751c13a58a9b907b54797c1e5452
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-17 18:41:24 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -40,7 +40,7 @@ def until_iterator(number_of_times)
   # code your solution here using the "until" keyword
   # hint: use a counter to tell the until loop when to stop!
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  until 
+  until
 end
 
 def for_iterator(number_of_times)
